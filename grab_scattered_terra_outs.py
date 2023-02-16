d07a46f4c0f3617a428743f573163bac67fd091e
aofarrel <aofarrel@ucsc.edu>
2023-02-15 16:32:19 -0800

    Grab cached and globbed files
    This might not work properly when there is a mix of cached and not cached

--- grab_scattered_terra_outs.py
+++ grab_scattered_terra_outs.py
@@ -4,20 +4,27 @@ import argparse
 arg = argparse.ArgumentParser(prog = 'grab scattered terra outs', 
 	description = "Download scattered workflow outputs when Terra's UI "
 	"isn't cooperating by directly evoking gsutil (REQUIRES gsutil to be set up and authenticated).")
-arg.add_argument('--bucket', default="fc-9f0bdb6d-155d-41bc-90af-35ef59252ff8", required=False)
-arg.add_argument('--submission_id', default="793a7888-6d9e-468d-a16c-e73a43562c91", required=False)
+arg.add_argument('--bucket', default="fc-caa84e5a-8ef7-434e-af9c-feaf6366a042", required=False)
+arg.add_argument('--submission_id', default="5f72a413-d152-4361-98fa-47de0c2606df", required=False)
 arg.add_argument('--workflow_name', default="myco", required=False)
-arg.add_argument('--workflow_id', default="21dab191-dc30-4897-bec8-f5a6e8bfd462", required=False)
+arg.add_argument('--workflow_id', default="e61073e1-e8b1-4554-8cf5-2caefd90481a", required=False)
 arg.add_argument('--task', default="make_mask_and_diff_", required=False)
-arg.add_argument('--file', default="*.sh", required=False)
+arg.add_argument('--cacheCopy', type=bool, default=True, required=False)
+arg.add_argument('--glob', type=bool, default=True, required=False)
+arg.add_argument('--file', default="*.diff", required=False)
 args = arg.parse_args()
-
 
 path = f"gs://{args.bucket}/submissions/{args.submission_id}/{args.workflow_name}/{args.workflow_id}/call-{args.task}/"
 shards = list(os.popen(f"gsutil ls {path}"))
 uris = []
+base = []
+if args.cacheCopy == True:
+	base.append("cacheCopy/")
+if args.glob == True:
+	base.append("glob*/")
+base.append(f"{args.file}")
 for shard in shards:
-	uri = shard[:-1] + f"{args.file}"
+	uri = shard[:-1] + "".join(base)
 	uris.append(uri)
 
 if len(uris) > 998:
