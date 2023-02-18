5ce56f62679b9e373365761ae8a06e9c2c8311ae
aofarrel <aofarrel@ucsc.edu>
2023-02-17 19:05:40 -0800

    Make debugging easier

--- grab_scattered_terra_outs.py
+++ grab_scattered_terra_outs.py
@@ -5,16 +5,17 @@ arg = argparse.ArgumentParser(prog = 'grab scattered terra outs',
 	description = "Download scattered workflow outputs when Terra's UI "
 	"isn't cooperating by directly evoking gsutil (REQUIRES gsutil to be set up and authenticated).")
 arg.add_argument('--bucket', default="fc-caa84e5a-8ef7-434e-af9c-feaf6366a042", required=False)
-arg.add_argument('--submission_id', default="5f72a413-d152-4361-98fa-47de0c2606df", required=False)
+arg.add_argument('--submission_id', required=True)
 arg.add_argument('--workflow_name', default="myco", required=False)
-arg.add_argument('--workflow_id', default="e61073e1-e8b1-4554-8cf5-2caefd90481a", required=False)
-arg.add_argument('--task', default="make_mask_and_diff_", required=False)
+arg.add_argument('--workflow_id', required=True)
+arg.add_argument('--task', default="make_mask_and_diff", required=False)
 arg.add_argument('--cacheCopy', type=bool, default=True, required=False)
 arg.add_argument('--glob', type=bool, default=True, required=False)
 arg.add_argument('--file', default="*.diff", required=False)
 args = arg.parse_args()
 
 path = f"gs://{args.bucket}/submissions/{args.submission_id}/{args.workflow_name}/{args.workflow_id}/call-{args.task}/"
+print(f"Constructed path:\n{path}")
 shards = list(os.popen(f"gsutil ls {path}"))
 uris = []
 base = []
@@ -23,6 +24,7 @@ if args.cacheCopy == True:
 if args.glob == True:
 	base.append("glob*/")
 base.append(f"{args.file}")
+
 for shard in shards:
 	uri = shard[:-1] + "".join(base)
 	uris.append(uri)
