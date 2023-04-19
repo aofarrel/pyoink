b35b5a482bd0fd8ba0d1d6ce1211d548c9ea9f34
Ash O'Farrell <aofarrel@ucsc.edu>
2023-04-19 11:36:11 -0700

    Fix typos

--- pyoink_myco.py
+++ pyoink_myco.py
@@ -21,7 +21,7 @@ args = parser.parse_args()
 # this is done in order of the smallest downloads first
 #### download pull report ####
 subprocess.check_call(f'python3 pyoink.py --submission_id {args.submission_id} --workflow_id {args.workflow_id} --file "pull_reports.txt" --task "cat_reports" --not_scattered', 
-                        shell=True, stdout=+sys.stdout, stderr=subprocess.STDOUT)
+                        shell=True, stdout=sys.stdout, stderr=subprocess.STDOUT)
 subprocess.run('mv downloaded_successfully.txt downloaded_successfully_report.txt', shell=True)
 subprocess.run('mv failed_to_download.txt failed_to_download_report.txt', shell=True)
 print("Finished pulling the SRA pull report file.")
@@ -40,13 +40,13 @@ subprocess.run('mv downloaded_successfully.txt downloaded_successfully_diff.txt'
 print("Finished pulling diffs.")
 
 #### download bedgraphs ####
-subprocess.check_call(f'python3 pyoink.py --submission_id {args.submission_id} --workflow_id {args.workflow_id} --file "*.bedgraph""', 
+subprocess.check_call(f'python3 pyoink.py --submission_id {args.submission_id} --workflow_id {args.workflow_id} --file "*.bedgraph"', 
                         shell=True, stdout=sys.stdout, stderr=subprocess.STDOUT)
 subprocess.run('mv downloaded_successfully.txt downloaded_successfully_bdgrph.txt', shell=True)
 print("Finished pulling bedgraphs.")
 
 #### download vcfs ####
-with subprocess.check_call(f'python3 pyoink.py --submission_id {args.submission_id} --workflow_id {args.workflow_id} --file "*.vcf" --task "varcall_with_array"', 
+subprocess.check_call(f'python3 pyoink.py --submission_id {args.submission_id} --workflow_id {args.workflow_id} --file "*.vcf" --task "varcall_with_array"', 
                         shell=True, stdout=sys.stdout, stderr=subprocess.STDOUT)
 subprocess.run('mv downloaded_successfully.txt downloaded_successfully_vcf.txt', shell=True)
 print("Finished pulling vcfs.")
