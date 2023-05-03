2893446a87b9f717bdafee57b02e77ec109b1a60
Ash O'Farrell <aofarrel@ucsc.edu>
2023-05-03 14:16:49 -0700

    Better way to verify files

--- pyoink_myco.py
+++ pyoink_myco.py
@@ -1,4 +1,6 @@
-# assumes pyoink.py is in the workdir
+#!/usr/bin/python3
+
+# assumes files get copied to workdir and pyoink.py is in ../helper_scripts
 # hardcoded for myco_sra
 
 import os 
@@ -19,61 +21,64 @@ parser.add_argument('--workflow_id', help="ID of workflow as it appears in Terra
 parser.add_argument('--bucket', default="fc-caa84e5a-8ef7-434e-af9c-feaf6366a042")
 args = parser.parse_args()
 
+subprocess.run('touch gs_info.txt', shell=True)
+subprocess.run(f'echo \"bucket: {args.bucket}\nsubmission_id: {args.submission_id}\nworkflow_id: {args.workflow_id}\n\" >> gs_info.txt', shell=True)
+
 # this is done in order of the smallest downloads first
 #### download reports ####
-subprocess.check_call(f'python3 pyoink.py --submission_id {args.submission_id} --bucket {args.bucket} --workflow_id {args.workflow_id} --file "pull_reports.txt" --task "cat_reports" --not_scattered', 
+subprocess.check_call(f'python3 ../helper_scripts/pyoink.py --submission_id {args.submission_id} --bucket {args.bucket} --workflow_id {args.workflow_id} --file "pull_reports.txt" --task "cat_reports" --not_scattered', 
                         shell=True, stdout=sys.stdout, stderr=subprocess.STDOUT)
 subprocess.run('mv downloaded_successfully.txt downloaded_successfully_pull.txt', shell=True)
 subprocess.run('mv failed_to_download.txt failed_to_download_pull.txt', shell=True)
 print("Finished pulling the SRA pull report file.")
 
-subprocess.check_call(f'python3 pyoink.py --submission_id {args.submission_id} --bucket {args.bucket} --workflow_id {args.workflow_id} --file "strain_reports.txt" --task "cat_strains" --not_scattered', 
+subprocess.check_call(f'python3 ../helper_scripts/pyoink.py --submission_id {args.submission_id} --bucket {args.bucket} --workflow_id {args.workflow_id} --file "strain_reports.txt" --task "cat_strains" --not_scattered', 
                         shell=True, stdout=sys.stdout, stderr=subprocess.STDOUT)
 subprocess.run('mv downloaded_successfully.txt downloaded_successfully_strain.txt', shell=True)
 subprocess.run('mv failed_to_download.txt failed_to_download_strain.txt', shell=True)
 print("Finished pulling the strain report file.")
 
-subprocess.check_call(f'python3 pyoink.py --submission_id {args.submission_id} --bucket {args.bucket} --workflow_id {args.workflow_id} --file "resistance_reports.txt" --task "cat_resistance" --not_scattered', 
+subprocess.check_call(f'python3 ../helper_scripts/pyoink.py --submission_id {args.submission_id} --bucket {args.bucket} --workflow_id {args.workflow_id} --file "resistance_reports.txt" --task "cat_resistance" --not_scattered', 
                         shell=True, stdout=sys.stdout, stderr=subprocess.STDOUT)
 subprocess.run('mv downloaded_successfully.txt downloaded_successfully_resistance.txt', shell=True)
 subprocess.run('mv failed_to_download.txt failed_to_download_resistance.txt', shell=True)
 print("Finished pulling the resistance report file.")
 
 ### download diff reports ####
-subprocess.check_call(f'python3 pyoink.py --submission_id {args.submission_id} --bucket {args.bucket} --workflow_id {args.workflow_id} --file "*.report"', 
+subprocess.check_call(f'python3 ../helper_scripts/pyoink.py --submission_id {args.submission_id} --bucket {args.bucket} --workflow_id {args.workflow_id} --file "*.report"', 
                         shell=True, stdout=sys.stdout, stderr=subprocess.STDOUT)
 subprocess.run('mv downloaded_successfully.txt downloaded_successfully_diffreports.txt', shell=True)
 subprocess.run('mv failed_to_download.txt failed_to_download_diffreports.txt', shell=True)
 print("Finished pulling the diff report files.")
 
 #### download tbprofiler jsons ####
-subprocess.check_call(f'python3 pyoink.py --submission_id {args.submission_id} --bucket {args.bucket} --workflow_id {args.workflow_id} --file "results/*.json" --task "profile"', 
+subprocess.check_call(f'python3 ../helper_scripts/pyoink.py --submission_id {args.submission_id} --bucket {args.bucket} --workflow_id {args.workflow_id} --file "results/*.json" --task "profile"', 
                         shell=True, stdout=sys.stdout, stderr=subprocess.STDOUT)
 subprocess.run('mv downloaded_successfully.txt downloaded_successfully_tbprf.txt', shell=True)
 subprocess.run('mv failed_to_download.txt failed_to_download_tbprf.txt', shell=True)
 print("Finished pulling TBProfiler JSONs.")
 
 #### download tbprofiler txts ####
-subprocess.check_call(f'python3 pyoink.py --submission_id {args.submission_id} --bucket {args.bucket} --workflow_id {args.workflow_id} --file "results/*.txt" --task "profile"', 
+subprocess.check_call(f'python3 ../helper_scripts/pyoink.py --submission_id {args.submission_id} --bucket {args.bucket} --workflow_id {args.workflow_id} --file "results/*.txt" --task "profile"', 
                         shell=True, stdout=sys.stdout, stderr=subprocess.STDOUT)
 subprocess.run('mv downloaded_successfully.txt downloaded_successfully_tbprftxt.txt', shell=True)
 subprocess.run('mv failed_to_download.txt failed_to_download_tbprftxt.txt', shell=True)
 print("Finished pulling TBProfiler text files.")
 
 #### download diffs ####
-subprocess.check_call(f'python3 pyoink.py --submission_id {args.submission_id} --bucket {args.bucket} --workflow_id {args.workflow_id}', 
+subprocess.check_call(f'python3 ../helper_scripts/pyoink.py --submission_id {args.submission_id} --bucket {args.bucket} --workflow_id {args.workflow_id}', 
                         shell=True, stdout=sys.stdout, stderr=subprocess.STDOUT)
 subprocess.run('mv downloaded_successfully.txt downloaded_successfully_diff.txt', shell=True)
 print("Finished pulling diffs.")
 
 #### download bedgraphs ####
-subprocess.check_call(f'python3 pyoink.py --submission_id {args.submission_id} --bucket {args.bucket} --workflow_id {args.workflow_id} --file "*.bedgraph"', 
+subprocess.check_call(f'python3 ../helper_scripts/pyoink.py --submission_id {args.submission_id} --bucket {args.bucket} --workflow_id {args.workflow_id} --file "*.bedgraph"', 
                         shell=True, stdout=sys.stdout, stderr=subprocess.STDOUT)
 subprocess.run('mv downloaded_successfully.txt downloaded_successfully_bdgrph.txt', shell=True)
 print("Finished pulling bedgraphs.")
 
 #### download vcfs ####
-subprocess.check_call(f'python3 pyoink.py --submission_id {args.submission_id} --bucket {args.bucket} --workflow_id {args.workflow_id} --file "*.vcf" --task "varcall_with_array"', 
+subprocess.check_call(f'python3 ../helper_scripts/pyoink.py --submission_id {args.submission_id} --bucket {args.bucket} --workflow_id {args.workflow_id} --file "*.vcf" --task "varcall_with_array"', 
                         shell=True, stdout=sys.stdout, stderr=subprocess.STDOUT)
 subprocess.run('mv downloaded_successfully.txt downloaded_successfully_vcf.txt', shell=True)
 print("Finished pulling vcfs.")
--- verify_files.py
+++ verify_files.py
@@ -3,30 +3,59 @@ import argparse
 
 arg = argparse.ArgumentParser(prog = 'pyoink verifier', 
 	description = "Verify you downloaded every sample's bits and pieces.")
+#arg.add_argument('-n', '--number_of_inputs', type=int)
 arg.add_argument('--vcf', type=bool, default=True)
 arg.add_argument('--diff', type=bool, default=True)
 arg.add_argument('--bedgraph', type=bool, default=True)
 arg.add_argument('--fastqc', type=bool, default=False)
-arg.add_argument('--number_of_inputs', type=int, default=1000)
+arg.add_argument('-e', '--exclude_these_samples', type=str, required=False)
+arg.add_argument('-i', '--input_samples_file', type=str)
 args = arg.parse_args()
 
 vcfs = []
 diffs = []
 bgs = []
+exclude = []
+all_samples = []
+samples_vcf = []
+samples_bgs = []
+samples_dif = []
+samples_missing = []
 ls = os.listdir()
+
+with open(f"{args.input_samples_file}", "r") as biosamples:
+	for line in biosamples.readlines():
+		all_samples.append(line)
+
+if args.exclude_these_samples:
+	with open(f"{args.exclude_these_samples}", "r") as nope:
+		for line in nope.readlines():
+			exclude.append(line)
+
+n_samples = len(all_samples) - len(exclude)
+samples = [sample for sample in all_samples if sample not in exclude]
+
 for file in ls:
 	if file.endswith(".vcf") and args.vcf is True:
 		vcfs.append(file)
+		samples_vcf.append(file.rstrip(".vcf"))
 	elif file.endswith(".diff") and args.diff is True:
 		diffs.append(file)
+		samples_dif.append(file.rstrip(".diff"))
 	elif file.endswith(".bedgraph") and args.bedgraph is True:
 		bgs.append(file)
+		samples_bgs.append(file.rstrip(".bedgraph"))
 if args.fastqc is True:
 	htmls = []
 	ls = os.listdir("additional_outputs/")
 	for file in ls:
 		if file.endswith(".html"):
 			htmls.append(file)
+
+samples.sort()
+samples_vcf.sort()
+samples_dif.sort()
+samples_bgs.sort()
 
 print(f"Total of {len(diffs)} diffs, {len(vcfs)} vcfs, {len(bgs)} bedgraphs.")
 if args.vcf is True and len(vcfs) != len(diffs):
@@ -35,3 +64,17 @@ if args.bedgraph is True and len(diffs) != len(bgs):
 	print(f"WARNING: {len(bgs)} bedGraphs, {len(diffs)} diffs")
 if args.fastqc is True and args.number_of_inputs - len(htmls) != len(diffs):
 	print(f"WARNING: Input {args.number_of_inputs}, but that minus {len(htmls)} doesn't match number of diffs ({len(diffs)})")
+
+for sample in samples:
+	if sample.strip('\n') in samples_dif:
+		pass
+	else:
+		samples_missing.append(sample)
+
+print(f"Total bioSample accessions: {len(all_samples)}")
+print(f"Samples (minus excluded): {len(samples)}")
+#print(f"Samples which can be excluded: {len(exclude)}")
+print(f"These are the {len(samples_missing)} samples are missing diff files:")
+for sample in samples_missing:
+	print(sample.strip('\n'))
+
